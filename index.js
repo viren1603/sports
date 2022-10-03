ff32ee36c1696c298c782a6d001130a7c7ed3a2b
viren <virenasodariya383536@gmail.com>
2022-10-03 13:58:17 +0530

    Complate insert update delate

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const app = express();
 const port = process.env.PORT;
 const bodyparser = require("body-parser");
 const sequlize = require("sequelize");
-
-app.set(bodyparser.json());
-app.set(bodyparser.urlencoded({ extended: false }))
+app.use(express.Router())
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }))
 
 
 let database = new sequlize(process.env.DB, process.env.DB_NAME, process.env.DB_PASSWORD, { port: 3306, dialect: "mysql" })
 
+database.authenticate().then(() => {
+    console.log("connection sucessfulley");
+}).catch((err) => {
+    console.log(err, 'db err');
+})
+
+
 const teamSchema = database.define('team', {
-    ids: sequlize.INTEGER,
+    ids: sequlize.STRING,
     name: sequlize.STRING
 })
 teamSchema.sync()
@@ -26,11 +33,75 @@ const playerSchema = database.define('player', {
 })
 playerSchema.sync()
 
-database.authenticate().then(() => {
-    console.log("connection sucessfulley");
-}).catch((err) => {
-    console.log(err, 'db err');
+
+
+app.post('/teamSchema', async function (req, res) {
+    // console.log({ data: req.body })
+    let ids = req.body.ids;
+    let name = req.body.name
+    let saveTeam = await teamSchema.build({
+        ids, name
+    })
+
+    await saveTeam.save()
+    res.send('data is posted')
 })
+app.post('/playerSchema', async function (req, res) {
+    let ids = req.body.ids;
+    let name = req.body.name
+    let age = req.body.age
+    let captain = req.body.captain
+    let dob = req.body.dob
+    let savePlayer = await playerSchema.build({
+        ids, name, age, captain, dob
+    })
+    await savePlayer.save()
+    res.end('data is posted')
+})
+
+app.get('/teamSchema', async function (req, res) {
+    let allData = await teamSchema.findAll();
+    res.send(allData)
+})
+app.get('/playerSchema', async function (req, res) {
+    let allData = await playerSchema.findAll();
+    res.send(allData)
+})
+
+
+
+app.put('/teamSchema/update/:id', async function (req, res) {
+    let data = req.body;
+    let a = teamSchema.update(data, {
+        where: { id: req.params.id }
+    })
+    res.send("data is update")
+})
+app.put('/playerSchema/update/:id', async function (req, res) {
+    let data = req.body;
+    let a = playerSchema.update(data, {
+        where: { id: req.params.id }
+    })
+    res.send("data is update")
+})
+
+app.delete('/teamSchema/delate/:id', async function (req, res) {
+    let a = await teamSchema.destroy({
+        where: { id: req.params.id }
+    })
+    res.send("data is delate")
+})
+
+app.delete('/playerSchema/delate/:id', async function (req, res) {
+    let a = await playerSchema.destroy({
+        where: { id: req.params.id }
+    })
+    res.send("data is delate")
+})
+
+
+
+
 
 app.listen(port, function (error) {
     if (error) {
