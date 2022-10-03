98b53cef0051d6ab2e7a402c68a85695e7916b63
viren <virenasodariya383536@gmail.com>
2022-10-03 10:25:08 +0530

    database connected

--- index.js
+++ index.js
@@ -3,11 +3,34 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT;
 const bodyparser = require("body-parser");
-
+const sequlize = require("sequelize");
 
 app.set(bodyparser.json());
 app.set(bodyparser.urlencoded({ extended: false }))
 
+
+let database = new sequlize(process.env.DB, process.env.DB_NAME, process.env.DB_PASSWORD, { port: 3306, dialect: "mysql" })
+
+const teamSchema = database.define('team', {
+    ids: sequlize.INTEGER,
+    name: sequlize.STRING
+})
+teamSchema.sync()
+
+const playerSchema = database.define('player', {
+    ids: sequlize.INTEGER,
+    name: sequlize.STRING,
+    age: sequlize.INTEGER,
+    captain: sequlize.STRING,
+    dob: sequlize.INTEGER
+})
+playerSchema.sync()
+
+database.authenticate().then(() => {
+    console.log("connection sucessfulley");
+}).catch((err) => {
+    console.log(err, 'db err');
+})
 
 app.listen(port, function (error) {
     if (error) {
